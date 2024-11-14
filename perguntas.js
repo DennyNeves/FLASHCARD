3ea4b9ed9204ea8dda3b2e992a94b25467d89346
DennyNeves <168123700+DennyNeves@users.noreply.github.com>
2024-11-14 11:04:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação!'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 2 + 2?',
+    '2 + 2 é igual a 4!'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)!'
+) 
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programação!'
+)
+
+criaCartao(
+    'Física',
+    'O que diz a 2ª Lei de Newton?',
+    'A 2ª Lei de Newton diz que: Fr = m . a!'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização!'
+)
+ 
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem que define a estrutura e o significado do conteúdo de uma página da web!'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris!'
+)
